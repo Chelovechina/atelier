02388f46f334e8059e85e13ea597118cfe4a0a8a
Chelovechina <zhkk2004@gmail.com>
2022-12-08 21:07:02 +0600

    feat: implement image section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import Atelier from './components/Atelier/Atelier';
 import Template from './components/Template/Template';
+import Image from './components/Image/Image';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main className="main">
         <Atelier />
         <Template />
+        <Image />
       </main>
     </>
   );
---
+++ src/components/Image/Image.jsx
@@ -0,0 +1,7 @@
+import styles from './Image.module.scss';
+
+const Image = () => {
+  return <section className={styles.image}></section>;
+};
+
+export default Image;
